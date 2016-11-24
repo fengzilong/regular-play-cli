2c86cd7ec0879fcef3461e005073bfceecdeaa61
mo <feng_zilong@163.com>
2016-11-24 19:28:52 +0800

    fixed #1

--- bin/play.js
+++ bin/play.js
@@ -10,15 +10,18 @@ program
 	.option( '-p, --port <port>', 'port' )
 	.option( '-e, --entry <entry>', 'entry' )
 	.option( '-d, --out-dir <dist>', 'dist' )
+	.option( '-t, --mobile-preview-template', '' )
 	.parse( process.argv );
 	;
 
 const port = program.port;
 const entry = program.entry;
 const dist = program.outDir;
+const mobilePreviewTemplate = program.mobilePreviewTemplate;
 
 main( {
 	port: port,
 	entry: entry,
 	dist: dist,
+	mobilePreviewTemplate: mobilePreviewTemplate,
 } );

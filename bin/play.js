6c4f5feadf6b30a1e4b33ec3d4c4adb12ae418de
mo <feng_zilong@163.com>
2017-03-06 23:47:43 +0800

    Add --preview-template and --resolve-fallback options

--- bin/play.js
+++ bin/play.js
@@ -10,18 +10,24 @@ program
 	.option( '-p, --port <port>', 'port' )
 	.option( '-e, --entry <entry>', 'path to entry file' )
 	.option( '-d, --out-dir <dist>', 'dist' )
-	.option( '-t, --mobile-preview-template <mobilePreviewTemplate>', 'preview template for mobile layout' )
+	.option( '-t, --preview-template <previewTemplate>', 'preview template for pc layout' )
+	.option( '-m, --mobile-preview-template <mobilePreviewTemplate>', 'preview template for mobile layout' )
+	.option( '-f, --resolve-fallback <resolveFallback>', 'fallback to resolve your dependencies' )
 	.parse( process.argv )
 	;
 
 const port = program.port;
 const entry = program.entry;
 const dist = program.outDir;
+const previewTemplate = program.previewTemplate;
 const mobilePreviewTemplate = program.mobilePreviewTemplate;
+const resolveFallback = program.resolveFallback;
 
 main( {
 	port: port,
 	entry: entry,
 	dist: dist,
+	previewTemplate: previewTemplate,
 	mobilePreviewTemplate: mobilePreviewTemplate,
+	resolveFallback: resolveFallback,
 } );
